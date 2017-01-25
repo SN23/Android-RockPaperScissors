d4d24be2cedef27677cd05c5ddf9578850543383
Sukhjinder <nahal.snahal@gmail.com>
2017-01-24 21:59:38 -0500

    Removed menu items, fixed action bar

--- app/src/main/java/com/sukhjinder/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/sukhjinder/rockpaperscissors/MainActivity.java
@@ -2,12 +2,12 @@ package com.sukhjinder.rockpaperscissors;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private int losses = 0;
     private int numOfRounds = 0;
     private int ties = 0;
-
 
 
     @Override
@@ -32,32 +31,37 @@ public class MainActivity extends AppCompatActivity {
         ImageButton PaperButton = (ImageButton) findViewById(R.id.paperButton);
         ImageButton ScissorsButton = (ImageButton) findViewById(R.id.scissorsButton);
         Button EndButton = (Button) findViewById(R.id.endButton);
+        final ImageView comChoice = (ImageView) findViewById(R.id.computerChoice);
 
 /**
  * Action listener for each of the image buttons
  */
-        if(RockButton!=null) {
+        if (RockButton != null) {
             RockButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
                     numOfRounds++;
                     String computerChoice = list.get(randomNumGenerator());
                     if (computerChoice.equals("rock")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "Tie", Toast.LENGTH_SHORT).show();
                         ties++;
+                        comChoice.setImageResource(R.drawable.rock);
                     }
 
                     if (computerChoice.equals("paper")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "You Win", Toast.LENGTH_SHORT).show();
                         wins++;
+                        comChoice.setImageResource(R.drawable.paper);
                     }
 
                     if (computerChoice.equals("scissors")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "You Lose", Toast.LENGTH_SHORT).show();
                         losses++;
+                        comChoice.setImageResource(R.drawable.scissors);
 
                     }
                 }
@@ -66,8 +70,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
-        if(PaperButton!=null) {
+        if (PaperButton != null) {
             PaperButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -77,26 +80,28 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "You Win", Toast.LENGTH_SHORT).show();
                         wins++;
+                        comChoice.setImageResource(R.drawable.rock);
                     }
 
                     if (computerChoice.equals("paper")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "Tie", Toast.LENGTH_SHORT).show();
                         ties++;
+                        comChoice.setImageResource(R.drawable.paper);
                     }
 
                     if (computerChoice.equals("scissors")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "You Lose", Toast.LENGTH_SHORT).show();
                         losses++;
+                        comChoice.setImageResource(R.drawable.scissors);
                     }
                 }
             });
         }
 
 
-
-        if(ScissorsButton!=null) {
+        if (ScissorsButton != null) {
             ScissorsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -106,25 +111,28 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "You Lose", Toast.LENGTH_SHORT).show();
                         losses++;
+                        comChoice.setImageResource(R.drawable.rock);
                     }
 
                     if (computerChoice.equals("paper")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "You Win", Toast.LENGTH_SHORT).show();
                         wins++;
+                        comChoice.setImageResource(R.drawable.paper);
                     }
 
                     if (computerChoice.equals("scissors")) {
                         Toast.makeText(getApplication().getBaseContext(),
                                 "Tie", Toast.LENGTH_SHORT).show();
                         ties++;
+                        comChoice.setImageResource(R.drawable.scissors);
                     }
                 }
             });
         }
 
 
-        if(EndButton!=null) {
+        if (EndButton != null) {
             EndButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -136,32 +144,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-//  Generates a number between 1 and 3 for to get Computer Choice
-    private int randomNumGenerator()
-    {
+    //  Generates a number between 1 and 3 for to get Computer Choice
+    private int randomNumGenerator() {
         double randomNum = Math.floor((Math.random() * 3));
-        return (int)randomNum;
+        return (int) randomNum;
     }
 }
